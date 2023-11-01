c9b1591b022bb3f0663c73874e9b979ec3d7b857
seitzdom <dominik.seitz@pasqal.com>
2023-11-01 18:02:57 +0100

    differentiate heas tests

--- tests/backends/test_adjoint.py
+++ tests/backends/test_adjoint.py
@@ -6,12 +6,15 @@ import torch
 from qadence.backends.api import backend_factory
 from qadence.blocks import AbstractBlock, chain
 from qadence.circuit import QuantumCircuit
+from qadence.constructors import hea
 from qadence.operations import CPHASE, RX, X, Z
 from qadence.parameters import VariationalParameter
 from qadence.types import DiffMode
 
+torch.use_deterministic_algorithms(True)
 
-@pytest.mark.parametrize("diff_mode", list(DiffMode))
+
+@pytest.mark.parametrize("diff_mode", [DiffMode.ADJOINT])
 def test_pyq_differentiation(diff_mode: str) -> None:
     batch_size = 1
     n_qubits = 2
@@ -33,7 +36,7 @@ def test_pyq_differentiation(diff_mode: str) -> None:
     assert torch.autograd.gradcheck(lambda y: func(inputs_x, y), inputs_y)
 
 
-@pytest.mark.parametrize("diff_mode", [DiffMode.AD, DiffMode.ADJOINT])
+@pytest.mark.parametrize("diff_mode", [DiffMode.ADJOINT])
 def test_scale_derivatives(diff_mode: str) -> None:
     batch_size = 1
     n_qubits = 1
@@ -51,3 +54,34 @@ def test_scale_derivatives(diff_mode: str) -> None:
         return bknd.expectation(pyqtorch_circ, pyqtorch_obs, all_params)
 
     assert torch.autograd.gradcheck(func, theta)
+
+
+@pytest.mark.flaky
+def test_hea_derivatives() -> None:
+    n_qubits = 2
+    observable: list[AbstractBlock] = [Z(0)]
+    block = hea(n_qubits, 1)
+    circ = QuantumCircuit(n_qubits, block)
+    theta_0_value = torch.rand(1, requires_grad=True)
+
+    def get_grad(theta: torch.Tensor, circ: QuantumCircuit, diff_mode: str) -> torch.Tensor:
+        bknd = backend_factory(backend="pyqtorch", diff_mode=diff_mode)
+        pyqtorch_circ, pyqtorch_obs, embeddings_fn, params = bknd.convert(circ, observable)
+        param_name = "theta_0"
+        params[param_name] = theta
+
+        def func(theta: torch.Tensor) -> torch.Tensor:
+            inputs = {param_name: theta}
+            all_params = embeddings_fn(params, inputs)
+            return bknd.expectation(pyqtorch_circ, pyqtorch_obs, all_params)
+
+        # assert torch.autograd.gradcheck(func, theta)
+        exp = bknd.expectation(
+            pyqtorch_circ, pyqtorch_obs, embeddings_fn(params, {param_name: theta})
+        )
+        dydtheta = torch.autograd.grad(exp, theta, torch.ones_like(exp))[0]
+        return dydtheta
+
+    ad_grad = get_grad(theta_0_value, circ, "ad")
+    adjoint_grad = get_grad(theta_0_value, circ, "adjoint")
+    assert torch.allclose(ad_grad, adjoint_grad, atol=0.1)
