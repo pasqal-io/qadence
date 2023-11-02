be91f5b637b8e8ba67802648b4912bb1792c6ab9
Nicolo <nicolo.toscano@pasqal.com>
2023-11-02 14:29:13 +0100

    initial commit for IdentityQNN

--- qadence/models/__init__.py
+++ qadence/models/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from .identity_qnn import IdentityQNN
 from .qnn import QNN
 from .quantum_model import QuantumModel
 
 # Modules to be automatically added to the qadence namespace
-__all__ = ["QNN", "QuantumModel"]
+__all__ = ["QNN", "QuantumModel", "IdentityQNN"]
