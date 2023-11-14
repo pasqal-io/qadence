1c34957f925bbe1e945c3a1edf6ec0aa28b22ed6
seitzdom <dominik.seitz@pasqal.com>
2023-11-14 11:29:41 +0100

    Import

--- qadence/backends/pyqtorch/backend.py
+++ qadence/backends/pyqtorch/backend.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from collections import Counter
 from dataclasses import dataclass, field
 from typing import Any
-from math import prod
 
 import pyqtorch as pyq
 import torch
