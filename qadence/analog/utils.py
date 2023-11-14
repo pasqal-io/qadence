75818dfc13155d1ee88c70d33bdcbee7eb99c725
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2023-11-14 14:16:24 +0100

    interaction hamiltonian file

--- qadence/analog/utils.py
+++ qadence/analog/utils.py
@@ -1,15 +1,5 @@
 from __future__ import annotations
 
-from math import dist as euclidean_distance
-
-from torch import Tensor, float64, tensor
-
-from qadence.analog.device import RydbergDevice
-from qadence.blocks.abstract import AbstractBlock
-from qadence.constructors import hamiltonian_factory
-from qadence.register import Register
-from qadence.types import Interaction
-
 # Ising coupling coefficient depending on the Rydberg level
 # Include a normalization to the Planck constant hbar
 # In units of [rad . µm^6 / µs]
@@ -67,46 +57,3 @@ C6_DICT = {
     99: 314098829.39,
     100: 352931119.11,
 }
-
-
-def _distance_all_qubits(r: Register) -> Tensor:
-    return tensor(
-        [euclidean_distance(r.coords[e[0]], r.coords[e[1]]) for e in r.all_edges], dtype=float64
-    )
-
-
-def _nn_strength(register: Register, rydberg_level: int) -> Tensor:
-    """(C_6 / R_ij**6)."""
-    c6 = C6_DICT[rydberg_level]
-    return c6 / (_distance_all_qubits(register) ** 6)
-
-
-def _xy_strength(register: Register, coeff_xy: float) -> Tensor:
-    """(C_3 / R_ij**3)."""
-    return coeff_xy / (_distance_all_qubits(register) ** 3)
-
-
-def rydberg_interaction_hamiltonian(device: RydbergDevice) -> AbstractBlock:
-    """
-    Computes the Rydberg Ising or XY interaction Hamiltonian for a register of qubits.
-
-    H_int = ∑_(j<i) (C_6 / R**6) * kron(N_i, N_j)
-
-    H_int = ∑_(j<i) (C_3 / R**3) * (kron(X_i, X_j) + kron(Y_i, Y_j))
-
-    Args:
-        register: the register of qubits.
-        interaction: the Interaction type.
-    """
-
-    if device.interaction == Interaction.NN:
-        strength_list = _nn_strength(device.register, device.rydberg_level)
-    elif device.interaction == Interaction.XY:
-        strength_list = _xy_strength(device.register, device.coeff_xy)
-
-    return hamiltonian_factory(
-        device.register,
-        interaction=device.interaction,
-        interaction_strength=strength_list,
-        use_complete_graph=True,
-    )
