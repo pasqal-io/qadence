9bdd471315f570b3aa9056ef8bf869cd8ba887c7
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2023-11-15 17:07:53 +0100

    fix entanglement

--- tests/backends/pulser_basic/test_entanglement.py
+++ tests/backends/pulser_basic/test_entanglement.py
@@ -2,28 +2,28 @@ from __future__ import annotations
 
 from collections import Counter
 
+import pytest
 import torch
 from metrics import JS_ACCEPTANCE
 
 from qadence import sample
 from qadence.backend import BackendName
-from qadence.backends.pulser.devices import Device, RealisticDevice
+from qadence.backends.pulser.devices import Device
 from qadence.blocks import chain
 from qadence.divergences import js_divergence
 from qadence.operations import RY, entangle
 from qadence.register import Register
 
+DEFAULT_SPACING = 8.0
 
-def test_entanglement() -> None:
+
+@pytest.mark.parametrize("device_type", [Device.IDEALIZED, Device.REALISTIC])
+def test_entanglement(device_type: Device) -> None:
     block = chain(entangle(1000, qubit_support=(0, 1)), RY(0, 3 * torch.pi / 2))
 
-    max_amp = RealisticDevice.channels["rydberg_global"].max_amp
-    weak_coupling_const = 1.2
-    spacing = weak_coupling_const * RealisticDevice.rydberg_blockade_radius(max_amp)
+    register = Register.line(2, scale=DEFAULT_SPACING)
 
-    register = Register(2, scale=spacing)
-
-    config = {"device_type": Device.REALISTIC}
+    config = {"device_type": device_type}
 
     res = sample(register, block, backend=BackendName.PULSER, n_shots=500, configuration=config)[0]
 
