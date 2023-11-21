a5317236b424289bc0dfcfd8c76cce9fb317b0d8
Roland Guichard <roland.guichard@pasqal.com>
2023-11-21 15:58:05 +0000

    Better imports.

--- qadence/states.py
+++ qadence/states.py
@@ -8,10 +8,10 @@ import torch
 from torch import Tensor, concat
 from torch.distributions import Categorical, Distribution
 
-from qadence.blocks import ChainBlock, KronBlock, PrimitiveBlock, chain, kron
+from qadence.blocks import ChainBlock, KronBlock, PrimitiveBlock
 from qadence.circuit import QuantumCircuit
 from qadence.execution import run
-from qadence.operations import CNOT, RX, RY, RZ, H, I, X
+from qadence.operations import CNOT, RX, RY, RZ, H, I, X, chain, kron
 from qadence.overlap import fidelity
 from qadence.types import BackendName, Endianness, StateGeneratorType
 from qadence.utils import basis_to_int
