b93b0e03deec700131f03dc9c34d45152d03aa12
Roland Guichard <roland.guichard@pasqal.com>
2023-11-22 13:28:41 +0000

    Correct flaky decorator.

--- tests/qadence/test_mitigation.py
+++ tests/qadence/test_mitigation.py
@@ -26,7 +26,7 @@ from qadence.types import BackendName, DiffMode
 pi = torch.pi
 
 
-@pytest.mark.flaky(max_run=5)
+@pytest.mark.flaky(max_runs=5)
 @pytest.mark.parametrize(
     "error_probability, n_shots, block, backend",
     [
