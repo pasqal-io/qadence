Sign wrong PSR

qadence/backends/horqrux/backend.py
@@ -70,11 +70,12 @@ class Backend(BackendInterface):
             state = tensor_to_jnp(pyqify(state)) if horqify_state else state
         state = circuit.native.forward(state, param_values)
         batch_size = 1  # FIXME : add batching
-        if unhorqify_state:
-            state = jarr_to_tensor(jnp.reshape(state, (batch_size, 2**circuit.abstract.n_qubits)))
         if endianness != self.native_endianness:
             state = jnp.reshape(state, (batch_size, 2**circuit.abstract.n_qubits))
             state = invert_endianness(jarr_to_tensor(state))
+        # TODO flatten state, invert endianness
+        if unhorqify_state:
+            state = state.ravel()
         return state
 
     def run_dm(
@@ -164,7 +165,6 @@ class Backend(BackendInterface):
             circuit=circuit,
             param_values=param_values,
             state=state,
-            endianness=self.native_endianness,
             horqify_state=True,
             unhorqify_state=False,
         )
@@ -177,10 +177,6 @@ class Backend(BackendInterface):
                 n_qubits=circuit.abstract.n_qubits,
             ),
         ]
-        if endianness != self.native_endianness:
-            from qadence.transpile.invert import invert_endianness
-
-            samples = invert_endianness(samples)
 
         return samples
 

qadence/engines/jax/differentiable_expectation.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Callable, Tuple, Union
+from typing import Any, Tuple, Union
 
 import jax
 import jax.numpy as jnp
@@ -13,7 +13,6 @@ from qadence.backend import Backend as QuantumBackend
 from qadence.backend import ConvertedCircuit, ConvertedObservable
 from qadence.backends.utils import (
     tensor_to_jnp,
-    values_to_jax,
 )
 from qadence.blocks.utils import uuid_to_eigen
 from qadence.measurements import Measurements
@@ -37,24 +36,12 @@ def is_leaf(subtree: Any) -> bool:
         return False
 
 
-def single_gap_psr(
-    expectation_fn: Callable[[dict[str, Array]], Array],
-    values: ParamDictType,
-    param_name: str,
-    spectral_gap: Array = jnp.array([2], dtype=jnp.float64),
-    shift: Array = jnp.array([jnp.pi / 2], dtype=jnp.float64),
-) -> Array:
-    # + pi/2 shift
-    shifted_values = values.copy()
-    shifted_values[param_name] = shifted_values[param_name] + shift
-    f_plus = expectation_fn(shifted_values)
-
-    # - pi/2 shift
-    shifted_values = values.copy()
-    shifted_values[param_name] = shifted_values[param_name] - shift
-    f_min = expectation_fn(shifted_values)
-
-    return spectral_gap * (f_plus - f_min) / (4 * jnp.sin(spectral_gap * shift / 2))
+def compute_gap(eigen_vals: Array) -> Array:
+    diffs = eigen_vals - eigen_vals.reshape(-1, 1)
+    sorted_unique_spectral_gaps = jnp.unique(jnp.abs(jnp.tril(diffs)))
+    # We have to filter out zeros
+    sorted_unique_spectral_gaps = sorted_unique_spectral_gaps[sorted_unique_spectral_gaps > 0]
+    return sorted_unique_spectral_gaps
 
 
 @dataclass
@@ -91,7 +78,7 @@ class JaxDifferentiableExpectation:
         def _expectation(state: Array, values: dict, uuid_to_eigen: dict) -> Array:
             return _expectation_fn(state, values, uuid_to_eigen)
 
-        values = values_to_jax(self.param_values)
+        values = self.param_values
         uuid_to_eigs = {
             k: tensor_to_jnp(v) for k, v in uuid_to_eigen(self.circuit.abstract.block).items()
         }
@@ -100,13 +87,14 @@ class JaxDifferentiableExpectation:
         def _expectation_fwd(state: Array, values: dict, uuid_to_eigen: dict) -> Any:
             return _expectation_fn(state, values, uuid_to_eigen), (state, values, uuid_to_eigen)
 
-        shift = jnp.array([jnp.pi / 2], dtype=jnp.float64)
+        shift = jnp.array(jnp.pi / 2, dtype=jnp.float64)
 
         def _expectation_bwd(res: Any, v: Array) -> Any:
             state, values, uuid_to_eigen = res
             grads = []
-            for param_name, spectral_gap in uuid_to_eigen.items():
+            for param_name, eigenvals in uuid_to_eigen.items():
                 # + pi/2 shift
+                spectral_gap = compute_gap(eigenvals)
                 shifted_values = values.copy()
                 shifted_values[param_name] = shifted_values[param_name] + shift
                 f_plus = _expectation_fn(state, shifted_values, uuid_to_eigen)
@@ -121,8 +109,7 @@ class JaxDifferentiableExpectation:
             grads = jax.tree_unflatten(
                 jax.tree_structure(self.circuit.native.operators, is_leaf=is_leaf), grads
             )
-            return *grads, None
+            return *grads, values, uuid_to_eigen
 
         _expectation.defvjp(_expectation_fwd, _expectation_bwd)
-        _exp_fn = jax.jit(jax.value_and_grad(_expectation))
-        return _exp_fn(self.state, values, uuid_to_eigs)
+        return _expectation(self.state, values, uuid_to_eigs)

tests/backends/horqrux/test_quantum_horqrux.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import jax.numpy as jnp
 import pytest
 import torch
+from jax import Array, value_and_grad
 
 from qadence import (
     CNOT,
@@ -13,22 +14,41 @@ from qadence import (
     RY,
     RZ,
     BackendName,
+    QuantumCircuit,
     X,
     Y,
     Z,
     expectation,
     run,
 )
+from qadence.backends import backend_factory
 from qadence.backends.utils import jarr_to_tensor
 from qadence.blocks import AbstractBlock
 from qadence.constructors import hea
 
 
 def test_psr() -> None:
-    values = {"theta": torch.tensor([torch.pi])}
-    psr = expectation(hea(2, 1), Z(0), values=values, backend="horqrux", diff_mode="gpsr")
-    ad = expectation(hea(2, 1), Z(0), values=values, backend="horqrux", diff_mode="ad")
-    assert jnp.allclose(psr[0], ad[0])
+    circ = QuantumCircuit(2, RX(0, "theta"))
+    v_list = []
+    grad_dict = {}
+    for diff_mode in ["ad", "gpsr"]:
+        hq_bknd = backend_factory("horqrux", diff_mode)
+        hq_circ, hq_obs, hq_fn, hq_params = hq_bknd.convert(circ, Z(0))
+        embedded_params = hq_fn(hq_params, {})
+        param_names = embedded_params.keys()
+        param_values = embedded_params.values()
+        param_array = jnp.array(jnp.concatenate([arr for arr in param_values]))
+
+        def _exp_fn(values: Array) -> Array:
+            vals = {k: v for k, v in zip(param_names, values)}
+            return hq_bknd.expectation(hq_circ, hq_obs, vals)
+
+        v, grads = value_and_grad(_exp_fn)(param_array)
+        v_list.append(v)
+        grad_dict[diff_mode] = grads
+    breakpoint()
+    print(grad_dict)
+    assert jnp.allclose(grad_dict["ad"], grad_dict["gpsr"])
 
 
 @pytest.mark.parametrize("block", [RX(0, 1.0), RY(0, 3.0), RZ(0, 4.0), X(0), Y(0), Z(0)])