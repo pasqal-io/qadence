4d3289220b1f74c87393acef1fe2d978be4c1d26
Mario Dagrada <mariodagrada24@gmail.com>
2023-11-30 17:48:24 +0100

    Analog feature maps

--- qadence/constructors/__init__.py
+++ qadence/constructors/__init__.py
@@ -23,6 +23,7 @@ from .hamiltonians import (
 )
 
 from .rydberg_hea import rydberg_hea, rydberg_hea_layer
+from .rydberg_feature_maps import rydberg_tower_feature_map, analog_feature_map
 
 from .qft import qft
 
@@ -45,4 +46,6 @@ __all__ = [
     "daqc_transform",
     "rydberg_hea",
     "rydberg_hea_layer",
+    "rydberg_tower_feature_map",
+    "analog_feature_map",
 ]
