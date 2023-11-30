86a14af610be53b1e3ae2214ac9181f35d5cadf6
raja_selvarajan <raja.selvarajan@pasqal.com>
2023-11-30 12:44:31 +0100

    adding mle implementation

--- qadence/mitigations/readout.py
+++ qadence/mitigations/readout.py
@@ -17,6 +17,32 @@ def corrected_probas(p_corr: npt.NDArray, T: npt.NDArray, p_raw: npt.NDArray) ->
     return norm(T @ p_corr.T - p_raw.T, ord=2) ** 2
 
 
+def mle_solve(p_raw: npt.NDArray) -> npt.NDArray:
+    ## sort p_raw by values while keeping track of indices
+    index_sort = p_raw.argsort()
+    p_sort = p_raw[index_sort]
+    neg_sum = 0
+    breakpoint = len(p_sort) - 1
+
+    for i in range(len(p_sort)):
+        ## if neg_sum cannot be distributed among other probabilities, continue to accumulate
+        if p_sort[i] + neg_sum / (len(p_sort) - i) < 0:
+            neg_sum += p_sort[i]
+            p_sort[i] = 0
+        # set breakpoint to current index
+        else:
+            breakpoint = i
+            break
+    ## number of entries to which i can distribute(includes breakpoint)
+    size = len(p_sort) - breakpoint
+    p_sort[breakpoint:] += neg_sum / size
+
+    re_index_sort = index_sort.argsort()
+    p_corr = p_sort[re_index_sort]
+
+    return p_corr
+
+
 def renormalize_counts(corrected_counts: npt.NDArray, n_shots: int) -> npt.NDArray:
     """Renormalize counts rounding discrepancies."""
     total_counts = sum(corrected_counts)
@@ -25,24 +51,19 @@ def renormalize_counts(corrected_counts: npt.NDArray, n_shots: int) -> npt.NDArr
         corrected_counts -= counts_diff
         corrected_counts = np.where(corrected_counts < 0, 0, corrected_counts)
         sum_corrected_counts = sum(corrected_counts)
-        if sum_corrected_counts < n_shots:
-            renormalization_factor = max(sum_corrected_counts, n_shots) / min(
-                sum_corrected_counts, n_shots
-            )
-        else:
-            renormalization_factor = min(sum_corrected_counts, n_shots) / max(
-                sum_corrected_counts, n_shots
-            )
+
+        renormalization_factor = n_shots / sum_corrected_counts
         corrected_counts = np.rint(corrected_counts * renormalization_factor).astype(int)
     return corrected_counts
 
 
 def mitigation_minimization(
-    noise: Noise, mitigation: Mitigations, samples: list[Counter]
+    noise: Noise, mitigation: Mitigations, samples: list[Counter], optimization_type: str
 ) -> list[Counter]:
     """Minimize a correction matrix subjected to stochasticity constraints.
 
     See Equation (5) in https://arxiv.org/pdf/2001.09980.pdf.
+    See Page(3) in https://arxiv.org/pdf/1106.5458.pdf for MLE implementation
     """
     noise_matrices = noise.options.get("noise_matrix", noise.options["confusion_matrices"])
     n_qubits = len(list(samples[0].keys())[0])
@@ -50,26 +71,46 @@ def mitigation_minimization(
     # Build the whole T matrix.
     T_matrix = reduce(torch.kron, noise_matrices).detach().numpy()
     corrected_counters: list[Counter] = []
+
+    if optimization_type == "mle":  ## MLE version
+        ##check if matrix is singular and use appropriate inverse
+        if np.linalg.matrix_rank(T_matrix) == T_matrix.shape[0]:
+            T_inv = np.linalg.inv(T_matrix)
+        else:
+            T_inv = np.linalg.pinv(T_matrix)
+
     for sample in samples:
         bitstring_length = 2**n_qubits
         # List of bitstrings in lexicographical order.
         ordered_bitstrings = [f"{i:0{n_qubits}b}" for i in range(bitstring_length)]
         # Array of raw probabilites.
         p_raw = np.array([sample[bs] for bs in ordered_bitstrings]) / n_shots
-        # Initial random guess in [0,1].
-        p_corr0 = np.random.rand(bitstring_length)
-        # Stochasticity constraints.
-        normality_constraint = LinearConstraint(
-            np.ones(bitstring_length).astype(int), lb=1.0, ub=1.0
-        )
-        positivity_constraint = LinearConstraint(
-            np.eye(bitstring_length).astype(int), lb=0.0, ub=1.0
-        )
-        constraints = [normality_constraint, positivity_constraint]
-        # Minimize the corrected probabilities.
-        res = minimize(corrected_probas, p_corr0, args=(T_matrix, p_raw), constraints=constraints)
-        # breakpoint()
-        corrected_counts = np.rint(res.x * n_shots).astype(int)
+
+        if optimization_type == "consrained_opt":
+            # Initial random guess in [0,1].
+            p_corr0 = np.random.rand(bitstring_length)
+            # Stochasticity constraints.
+            normality_constraint = LinearConstraint(
+                np.ones(bitstring_length).astype(int), lb=1.0, ub=1.0
+            )
+            positivity_constraint = LinearConstraint(
+                np.eye(bitstring_length).astype(int), lb=0.0, ub=1.0
+            )
+            constraints = [normality_constraint, positivity_constraint]
+            # Minimize the corrected probabilities.
+            res = minimize(
+                corrected_probas, p_corr0, args=(T_matrix, p_raw), constraints=constraints
+            )
+            p_corr = res.x
+
+        elif optimization_type == "mle":  ## MLE version
+            ## compute corrected inverse using matrix inversion and run MLE
+            p_corr = mle_solve(T_inv @ p_raw)
+        else:
+            raise NotImplementedError
+
+        corrected_counts = np.rint(p_corr * n_shots).astype(int)
+
         # Renormalize if total counts differs from n_shots.
         corrected_counts = renormalize_counts(corrected_counts=corrected_counts, n_shots=n_shots)
         # At this point, the count should be off by at most 2, added or substracted to/from the
@@ -89,5 +130,9 @@ def mitigation_minimization(
     return corrected_counters
 
 
-def mitigate(noise: Noise, mitigation: Mitigations, samples: list[Counter]) -> list[Counter]:
-    return mitigation_minimization(noise=noise, mitigation=mitigation, samples=samples)
+def mitigate(
+    noise: Noise, mitigation: Mitigations, samples: list[Counter], optimization_type: str = "mle"
+) -> list[Counter]:
+    return mitigation_minimization(
+        noise=noise, mitigation=mitigation, samples=samples, optimization_type=optimization_type
+    )
