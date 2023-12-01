0872c6e82332295526c2b99d85c1ec1ec79e161d
seitzdom <dominik.seitz@pasqal.com>
2023-12-01 17:18:15 +0100

    Update loop

--- examples/backends/low_level/horqrux_backend.py
+++ examples/backends/low_level/horqrux_backend.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import jax.numpy as jnp
 import optax
-from jax import Array, grad, jit
+from jax import Array, jit, value_and_grad
 
 from qadence.backends import backend_factory
 from qadence.circuit import QuantumCircuit
@@ -35,20 +35,33 @@ for diff_mode in ["ad", "gpsr"]:
         new_params = optax.apply_updates(params, updates)
         return new_params, opt_state
 
-    @jit
     def _exp_fn(values: Array) -> Array:
         vals = {k: v for k, v in zip(param_names, values)}
         return hq_bknd.expectation(hq_circ, hq_obs, vals)
 
     init_pred = _exp_fn(init_array)
 
-    for _ in range(num_epochs):
-        grads = grad(_exp_fn)(param_array)
-        param_array, opt_state = optimize_step(param_array, opt_state, grads)
+    def _loss(param_arr: Array, y_true: Array) -> Array:
+        expval = _exp_fn(param_arr)
+        return (expval - y_true) ** 2
+
+    loss_and_grads = value_and_grad(_loss)
+
+    def _train_step(param_array: Array, opt_state, y_true: Array) -> tuple:
+        loss, grads = loss_and_grads(param_array, y_true)
+        updated_param_array, updated_opt_state = optimize_step(param_array, opt_state, grads)
+        return loss, updated_param_array, updated_opt_state
+
+    train_step = jit(_train_step)
+    y_true = jnp.array(1.0, dtype=jnp.float64)  # We need to enforce float64
+
+    for epoch in range(num_epochs):
+        loss, param_array, opt_state = train_step(param_array, opt_state, y_true)
+        print(f"epoch {epoch} loss:{loss}")
 
     final_pred = _exp_fn(param_array)
 
     print(f"diff_mode '{diff_mode}, Initial prediction:")
     print(init_pred)
-    print(f"diff_mode '{diff_mode}, final prediction:")
+    print(f"diff_mode '{diff_mode}, Final prediction:")
     print(final_pred)
