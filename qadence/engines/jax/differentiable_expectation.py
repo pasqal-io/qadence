febe3cb5cc4d39c832ceb9ed8b4fb928804551ce
seitzdom <dominik.seitz@pasqal.com>
2023-12-06 19:07:36 +0100

    Experimental full vmap PSR

--- qadence/engines/jax/differentiable_expectation.py
+++ qadence/engines/jax/differentiable_expectation.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from operator import add, sub
 from typing import Any, Tuple
 
 import jax.numpy as jnp
@@ -88,6 +89,44 @@ class DifferentiableExpectation:
                 f_plus, f_min = shift_circ(param_name, values)
                 grad = spectral_gap * (f_plus - f_min) / (4.0 * jnp.sin(spectral_gap * shift / 2.0))
                 grads[param_name] = jnp.sum(tangent * grad, axis=1) if n_obs > 1 else tangent * grad
+            return None, None, grads
+
+        ## TODO ask GJ/Atiyo: Experimental vmap version which generates an array of all shift permutations and vmaps the circ
+        def expectation_bwd_vmap(
+            res: Tuple[Array, ParamDictType, ParamDictType], tangent: Array
+        ) -> Any:
+            state, values, psr_params = res
+            grads = {}
+            # Hardcoding the single spectral_gap to 2. for jax.lax jitting reasons.
+            spectral_gap = 2.0
+            shift = jnp.pi / 2
+            psr_names = list(psr_params.keys())
+            psr_values = jnp.array([arr for arr in psr_params.values()]).reshape(1, len(psr_names))
+            _stacked_vals = []
+            for i in range(len(psr_names)):
+                for op in [add, sub]:
+                    shift_val = op(psr_values[0, i], shift)
+                    shifted_values = jnp.copy(psr_values)
+                    shifted_values = shifted_values.at[0, i].set(shift_val)
+                    _stacked_vals.append(shifted_values)
+            stacked_vals = jnp.concatenate(_stacked_vals, axis=0)
+            non_psr_params = {k: v for k, v in values.items() if k not in psr_params.keys()}
+
+            def eval_circ(values: Array) -> Array:
+                shifted_params = {k: v for k, v in zip(psr_names, values)}
+                shifted_params.update(non_psr_params)
+                return expectation(state, shifted_params, psr_params)
+
+            exps = vmap(eval_circ, in_axes=(0,))(stacked_vals)
+
+            def psr_grad(f_plus: Array, f_min: Array, spectral_gap: Array = 2.0) -> Array:
+                grad = spectral_gap * (f_plus - f_min) / (4.0 * jnp.sin(spectral_gap * shift / 2.0))
+                return jnp.sum(tangent * grad, axis=1) if n_obs > 1 else tangent * grad
+
+            grads = {
+                param_name: psr_grad(f_plus, f_min, spectral_gap)
+                for param_name, (f_plus, f_min) in zip(psr_names, zip(exps[0::2], exps[1::2]))
+            }
             return None, None, grads
 
         expectation.defvjp(expectation_fwd, expectation_bwd)
