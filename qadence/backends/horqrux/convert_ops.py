4a4b4f18aab94eaa5836dab27125fd1b1b10ce54
seitzdom <dominik.seitz@pasqal.com>
2024-01-29 10:55:22 +0100

    Refac convert_ops

--- qadence/backends/horqrux/convert_ops.py
+++ qadence/backends/horqrux/convert_ops.py
@@ -18,9 +18,7 @@ from jax.tree_util import register_pytree_node_class
 from qadence.blocks import (
     AbstractBlock,
     AddBlock,
-    ChainBlock,
     CompositeBlock,
-    KronBlock,
     ParametricBlock,
     PrimitiveBlock,
     ScaleBlock,
@@ -62,21 +60,18 @@ class HorqruxCircuit:
     def tree_unflatten(cls, aux_data: Any, children: Any) -> Any:
         return cls(*children, *aux_data)
 
+    def _forward(self, state: Array, values: ParamDictType) -> Array:
+        return reduce(lambda state, gate: gate.forward(state, values), self.operators, state)
+
     def forward(self, state: Array, values: ParamDictType) -> Array:
-        for op in self.operators:
-            state = op.forward(state, values)
-        return state
+        return self._forward(state, values)
 
 
+@register_pytree_node_class
 @dataclass
 class HorqruxObservable(HorqruxCircuit):
     def __init__(self, operators: list[Gate]):
         super().__init__(operators=operators)
-
-    def _forward(self, state: Array, values: ParamDictType) -> Array:
-        for op in self.operators:
-            state = op.forward(state, values)
-        return state
 
     def forward(self, state: Array, values: ParamDictType) -> Array:
         return overlap(state, self._forward(state, values))
@@ -85,8 +80,8 @@ class HorqruxObservable(HorqruxCircuit):
 def convert_observable(
     block: AbstractBlock, n_qubits: int, config: Configuration
 ) -> HorqruxObservable:
-    _ops = convert_block(block, n_qubits, config)
-    return HorqruxObservable(_ops)
+    ops = convert_block(block, n_qubits, config)
+    return HorqruxObservable(ops)
 
 
 def convert_block(
@@ -97,68 +92,28 @@ def convert_block(
     ops = []
     if isinstance(block, CompositeBlock):
         ops = list(flatten(*(convert_block(b, n_qubits, config) for b in block.blocks)))
-        if isinstance(block, AddBlock):
-            ops = [HorqAddGate(ops)]
-        elif isinstance(block, ChainBlock):
-            ops = [HorqruxCircuit(ops)]
-        elif isinstance(block, KronBlock):
-            if all(
-                [
-                    isinstance(b, ParametricBlock) and not isinstance(b, ScaleBlock)
-                    for b in block.blocks
-                ]
-            ):
-                param_names = [config.get_param_name(b)[0] for b in block.blocks if b.is_parametric]
-                ops = [
-                    HorqKronParametric(
-                        gates=[ops_map[b.name] for b in block.blocks],
-                        target=[b.qubit_support[0] for b in block.blocks],
-                        param_names=param_names,
-                    )
-                ]
-
-            elif all([b.name == "CNOT" for b in block.blocks]):
-                ops = [
-                    HorqKronCNOT(
-                        gates=[ops_map[b.name] for b in block.blocks],
-                        target=[b.qubit_support[1] for b in block.blocks],
-                        control=[b.qubit_support[0] for b in block.blocks],
-                    )
-                ]
-            else:
-                ops = [HorqruxCircuit(ops)]
-
-    elif isinstance(block, CNOT):
-        native_op = ops_map[block.name]
-        ops = [
-            HorqCNOTGate(native_op, block.qubit_support[0], block.qubit_support[1])
-        ]  # in horqrux target and control are swapped
-
-    elif isinstance(block, (CRX, CRY, CRZ)):
-        native_op_fn = ops_map[block.name]
-        param_name = config.get_param_name(block)[0]
-        control, target = block.qubit_support[0], block.qubit_support[1]
-        ops = [
-            HorqParametricGate(gate=native_op_fn(target=target, control=control, param=param_name))
-        ]
+        ops = [HorqAddGate(ops)] if isinstance(block, AddBlock) else [HorqruxCircuit(ops)]
     elif isinstance(block, ScaleBlock):
         op = convert_block(block.block, n_qubits, config=config)[0]
         param_name = config.get_param_name(block)[0]
         ops = [HorqScaleGate(op, param_name)]
-
-    elif isinstance(block, ParametricBlock):
+    elif block.name in ops_map.keys():
         native_op_fn = ops_map[block.name]
-        if len(block.parameters._uuid_dict) > 1:
-            raise NotImplementedError("Only single-parameter operations are supported.")
-        param_name = config.get_param_name(block)[0]
-        ops = [
-            HorqParametricGate(gate=native_op_fn(param=param_name, target=block.qubit_support[0]))
-        ]
+        target, control = (
+            (block.qubit_support[1], block.qubit_support[0])
+            if isinstance(block, (CNOT, CRX, CRY, CRZ))
+            else (block.qubit_support[0], (None,))
+        )
+        native_gate: Gate
+        if isinstance(block, ParametricBlock):
+            if len(block.parameters._uuid_dict) > 1:
+                raise NotImplementedError("Only single-parameter operations are supported.")
+            param_name = config.get_param_name(block)[0]
+            native_gate = native_op_fn(param=param_name, target=target, control=control)
 
-    elif isinstance(block, PrimitiveBlock):
-        native_op = ops_map[block.name]
-        target = block.qubit_support[0]
-        ops = [HorqPrimitiveGate(gate=native_op(target=target))]
+        elif isinstance(block, PrimitiveBlock):
+            native_gate = native_op_fn(target=target, control=control)
+        ops = [HorqOperation(native_gate)]
 
     else:
         raise NotImplementedError(f"Non-supported operation of type {type(block)}.")
@@ -166,80 +121,53 @@ def convert_block(
     return ops
 
 
-class HorqPrimitiveGate:
-    def __init__(self, gate: Gate):
-        self.gate: Gate = gate
-
-    def forward(self, state: Array, values: ParamDictType) -> Array:
-        return apply_gate(state, self.gate)
-
-
-class HorqCNOTGate:
-    def __init__(self, gate: Gate, control: int, target: int):
-        self.gates: Callable = gate
-        self.control: int = control
-        self.target: int = target
-
-    def forward(self, state: Array, values: ParamDictType) -> Array:
-        return apply_gate(state, self.gates(self.target, self.control))
-
-
-class HorqKronParametric:
-    def __init__(self, gates: list[Gate], param_names: list[str], target: list[int]):
-        self.operators: list[Gate] = gates
-        self.target: list[int] = target
-        self.param_names: list[str] = param_names
-
-    def forward(self, state: Array, values: ParamDictType) -> Array:
-        return apply_gate(
-            state,
-            tuple(
-                gate(values[param_name], target)
-                for gate, target, param_name in zip(self.operators, self.target, self.param_names)
-            ),
-        )
-
-
-class HorqKronCNOT(HorqruxCircuit):
-    def __init__(self, gates: list[Gate], target: list[int], control: list[int]):
-        self.operators: list[Gate] = gates
-        self.target: list[int] = target
-        self.control: list[int] = control
-
-    def forward(self, state: Array, values: ParamDictType) -> Array:
-        return apply_gate(
-            state,
-            tuple(
-                gate(target, control)
-                for gate, target, control in zip(self.operators, self.target, self.control)
-            ),
-        )
-
-
-class HorqParametricGate:
-    def __init__(self, gate: Gate):
-        self.gate: Any = gate
-
-    def forward(self, state: Array, values: ParamDictType) -> Array:
-        return apply_gate(state, self.gate, values)
-
-
+@register_pytree_node_class
 class HorqAddGate(HorqruxCircuit):
     def __init__(self, operations: list[Gate]):
         self.operators = operations
         self.name = "Add"
 
     def forward(self, state: Array, values: ParamDictType = {}) -> Array:
-        return reduce(add, (op.forward(state, values) for op in self.operators))
+        return reduce(add, (gate.forward(state, values) for gate in self.operators))
 
     def __repr__(self) -> str:
         return self.name + f"({self.operators})"
 
 
+@register_pytree_node_class
+@dataclass
+class HorqOperation:
+    def __init__(self, native_gate: Gate):
+        self.native_gate = native_gate
+
+    def forward(self, state: Array, values: ParamDictType) -> Array:
+        return apply_gate(state, self.native_gate, values)
+
+    def tree_flatten(self) -> tuple[tuple[Gate], tuple[()]]:
+        children = (self.native_gate,)
+        aux_data = ()
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data: Any, children: Any) -> Any:
+        return cls(*children, *aux_data)
+
+
+@register_pytree_node_class
+@dataclass
 class HorqScaleGate:
-    def __init__(self, op: Gate, parameter_name: str):
-        self.op = op
+    def __init__(self, gate: HorqOperation, parameter_name: str):
+        self.gate = gate
         self.parameter: str = parameter_name
 
     def forward(self, state: Array, values: ParamDictType) -> Array:
-        return jnp.array(values[self.parameter]) * self.op.forward(state, values)
+        return jnp.array(values[self.parameter]) * self.gate.forward(state, values)
+
+    def tree_flatten(self) -> tuple[tuple[HorqOperation], tuple[str]]:
+        children = (self.gate,)
+        aux_data = (self.parameter,)
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data: Any, children: Any) -> Any:
+        return cls(*children, *aux_data)
