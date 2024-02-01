b6dcb0ef86ba29fa9e6fa386036bdb7c0a536b45
Roland Guichard <roland.guichard@pasqal.com>
2024-02-01 14:52:38 +0000

    Store the noise model when passed to sample method.

--- qadence/models/quantum_model.py
+++ qadence/models/quantum_model.py
@@ -185,6 +185,8 @@ class QuantumModel(nn.Module):
         params = self.embedding_fn(self._params, values)
         if noise is None:
             noise = self._noise
+        else:
+            self._noise = noise
         if mitigation is None:
             mitigation = self._mitigation
         return self.backend.sample(
