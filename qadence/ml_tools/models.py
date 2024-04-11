[Fix] Correctly set precision in torch TransformedModule (#388)

qadence/ml_tools/models.py
@@ -289,11 +289,23 @@ class TransformedModule(torch.nn.Module):
     def to(self, *args: Any, **kwargs: Any) -> TransformedModule:
         try:
             self.model = self.model.to(*args, **kwargs)
-            self._input_scaling = self._input_scaling.to(*args, **kwargs)
-            self._input_shifting = self._input_shifting.to(*args, **kwargs)
-            self._output_scaling = self._output_scaling.to(*args, **kwargs)
-            self._output_shifting = self._output_shifting.to(*args, **kwargs)
+            if isinstance(self.model, QuantumModel):
+                device = self.model._circuit.native.device
+                dtype = (
+                    torch.float64
+                    if self.model._circuit.native.dtype == torch.cdouble
+                    else torch.float32
+                )
 
+                self._input_scaling = self._input_scaling.to(device=device, dtype=dtype)
+                self._input_shifting = self._input_shifting.to(device=device, dtype=dtype)
+                self._output_scaling = self._output_scaling.to(device=device, dtype=dtype)
+                self._output_shifting = self._output_shifting.to(device=device, dtype=dtype)
+            elif isinstance(self.model, torch.nn.Module):
+                self._input_scaling = self._input_scaling.to(*args, **kwargs)
+                self._input_shifting = self._input_shifting.to(*args, **kwargs)
+                self._output_scaling = self._output_scaling.to(*args, **kwargs)
+                self._output_shifting = self._output_shifting.to(*args, **kwargs)
             logger.debug(f"Moved {self} to {args}, {kwargs}.")
         except Exception as e:
             logger.warning(f"Unable to move {self} to {args}, {kwargs} due to {e}.")

qadence/ml_tools/train_grad.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Callable, Union
 
 from rich.progress import BarColumn, Progress, TaskProgressColumn, TextColumn, TimeRemainingColumn
+from torch import complex128, float32, float64
 from torch import device as torch_device
 from torch import dtype as torch_dtype
 from torch.nn import DataParallel, Module
@@ -110,17 +111,17 @@ def train(
     train_with_grad(model, data, optimizer, config, loss_fn=loss_fn)
     ```
     """
+    # load available checkpoint
+    init_iter = 0
+    if config.folder:
+        model, optimizer, init_iter = load_checkpoint(config.folder, model, optimizer)
+        logger.debug(f"Loaded model and optimizer from {config.folder}")
 
     # Move model to device before optimizer is loaded
     if isinstance(model, DataParallel):
         model = model.module.to(device=device, dtype=dtype)
     else:
         model = model.to(device=device, dtype=dtype)
-    # load available checkpoint
-    init_iter = 0
-    if config.folder:
-        model, optimizer, init_iter = load_checkpoint(config.folder, model, optimizer)
-        logger.debug(f"Loaded model and optimizer from {config.folder}")
     # initialize tensorboard
     writer = SummaryWriter(config.folder, purge_step=init_iter)
 
@@ -131,7 +132,9 @@ def train(
         TaskProgressColumn(),
         TimeRemainingColumn(elapsed_when_finished=True),
     )
-
+    data_dtype = None
+    if dtype:
+        data_dtype = float64 if dtype == complex128 else float32
     with progress:
         dl_iter = iter(dataloader) if dataloader is not None else None
 
@@ -143,7 +146,12 @@ def train(
                 # which do not have classical input data (e.g. chemistry)
                 if dataloader is None:
                     loss, metrics = optimize_step(
-                        model=model, optimizer=optimizer, loss_fn=loss_fn, xs=None, device=device
+                        model=model,
+                        optimizer=optimizer,
+                        loss_fn=loss_fn,
+                        xs=None,
+                        device=device,
+                        dtype=data_dtype,
                     )
                     loss = loss.item()
 
@@ -154,6 +162,7 @@ def train(
                         loss_fn=loss_fn,
                         xs=next(dl_iter),  # type: ignore[arg-type]
                         device=device,
+                        dtype=data_dtype,
                     )
 
                 else: