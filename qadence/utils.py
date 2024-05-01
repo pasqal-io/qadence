172013e6f4a66d7cbd1767a53901ed2e57b379a8
João P. Moutinho <56390829+jpmoutinho@users.noreply.github.com>
2024-05-01 16:28:32 +0200

    actually fix rounding

--- qadence/utils.py
+++ qadence/utils.py
@@ -155,16 +155,16 @@ def format_number(x: float | complex, num_digits: int = 3) -> str:
 
 
 def format_parameter(p: sympy.Basic) -> str:
-    n_round = 3  # Number of decimal cases
+    # Number of digits to round to
+    num_digits = 3
 
     def round_expr(expr: sympy.Basic, num_digits: int) -> sympy.Basic:
         return expr.xreplace({n: round(n, num_digits) for n in expr.atoms(sympy.Number)})
 
-    conv_str = str(round_expr(p, n_round))
-
-    if len(conv_str) > n_round + 2:
-        conv_str = conv_str[0 : n_round + 2]
-
+    expr = round_expr(p, num_digits)
+    conv_str = str(expr)
+    if expr.is_real and len(conv_str) > num_digits + 2:
+        conv_str = conv_str[0 : num_digits + 2]
     return conv_str
 
 
