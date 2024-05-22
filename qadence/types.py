1ace889d6bb184e2d235a492388b027c2e04ace7
seitzdom <dominik.seitz@pasqal.com>
2024-05-22 11:35:52 +0200

    [Fix] Allow numpy parameter values

--- qadence/types.py
+++ qadence/types.py
@@ -9,7 +9,7 @@ import sympy
 from numpy.typing import ArrayLike
 from torch import Tensor, pi
 
-TNumber = Union[int, float, complex]
+TNumber = Union[int, float, complex, np.int64, np.float64]
 """Union of python number types."""
 
 TDrawColor = Tuple[float, float, float, float]
