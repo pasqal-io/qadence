dad5648186108353a220eadd9ffcbd7cd4e3767c
seitzdom <dominik.seitz@pasqal.com>
2024-06-05 12:48:52 +0200

    global nqubits

--- qadence/backends/pulser/convert_ops.py
+++ qadence/backends/pulser/convert_ops.py
@@ -26,24 +26,24 @@ class PulserObservable(Module):
     def __init__(self, block: AbstractBlock, n_qubits: int | None):
         super().__init__()
         self.block = block
-        self.n_qubits = n_qubits
+        self.n_qubits = n_qubits if n_qubits else max(block.qubit_support) + 1
 
         if not self.block.is_parametric:
-            block_mat = block_to_tensor(self.block, {}, qubit_support=block.qubit_support).squeeze(
-                0
-            )
+            block_mat = block_to_tensor(
+                self.block, {}, qubit_support=tuple(i for i in range(self.n_qubits))
+            ).squeeze(0)
             self.register_buffer("block_mat", block_mat)
 
     def forward(
         self,
         state: torch.Tensor,
-        values: dict[str, torch.Tensor] | list = {},
+        values: dict[str, torch.Tensor] = dict(),
         qubit_support: tuple | None = None,
         endianness: Endianness = Endianness.BIG,
     ) -> torch.Tensor:
         if self.block.is_parametric:
             block_mat = block_to_tensor(
-                self.block, values, qubit_support=qubit_support, endianness=endianness  # type: ignore [arg-type]  # noqa
+                self.block, values, qubit_support=qubit_support, endianness=endianness
             ).squeeze(0)
         else:
             block_mat = self.block_mat
