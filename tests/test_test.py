db16ee42eea6bae00c09a751884747d6afc72943
ducthanh1991 <ducttran.pro@gmail.com>
2024-06-05 08:35:05 +0200

    [20240605.2] use decorator in the test

--- tests/test_test.py
+++ tests/test_test.py
@@ -3,24 +3,47 @@ from pyqtorch.primitive import X,Y,Z,CZ,CX,CY
 from pyqtorch.utils import product_state
 from pyqtorch.apply import apply_operator
 from torch import equal
+from itertools import product
+import pytest
+
+# def test_PyQComposedBlock():
+#     values = None
+#     ops = [CX(target=0,control=1),CX(target=1,control=0),X(0),X(2),CX(target=0,control=2),CY(target=0,control=2)]
+#     composed_block = PyQComposedBlock( ops = ops, qubits=[0,1,2],n_qubits=3)
+
+#     qubits_strings = [str(i)+str(j)+str(k) for i in range(2) for j in range(2) for k in range(2)]
+
+#     for qubits_string in qubits_strings:
+#         state = product_state(qubits_string)
+        
+#         composed_state = composed_block.forward(state=state,values=values)
+
+#         state_ev=state
+#         for op in ops:
+#             state_ev = apply_operator(state_ev,op.unitary(values=values),op.qubit_support)
+        
+        
+#         assert equal(composed_state,state_ev)
 
 
-def test_PyQComposedBlock():
+@pytest.mark.parametrize('ops,state',product([[CX(target=0,control=1),CX(target=1,control=0),X(0),X(2),CX(target=0,control=2),CY(target=0,control=2)]],
+                                             [product_state(str(i)+str(j)+str(k)) for i in range(2) for j in range(2) for k in range(2)]
+                                             )
+                        )
+
+def test_PyQComposedBlock(ops,state):
     values = None
-    ops = [CX(target=0,control=1),CX(target=1,control=0),X(0),X(2),CX(target=0,control=2),CY(target=0,control=2)]
+    
     composed_block = PyQComposedBlock( ops = ops, qubits=[0,1,2],n_qubits=3)
+    composed_state = composed_block.forward(state=state,values=values)
+    state_ev=state
+    for op in ops:
+        state_ev = apply_operator(state_ev,op.unitary(values=values),op.qubit_support)
+    
 
-    qubits_strings = [str(i)+str(j)+str(k) for i in range(2) for j in range(2) for k in range(2)]
-
-    for qubits_string in qubits_strings:
-        state = product_state(qubits_string)
-        
-        composed_state = composed_block.forward(state=state,values=values)
-
-        state_ev=state
-        for op in ops:
-            state_ev = apply_operator(state_ev,op.unitary(values=values),op.qubit_support)
+   
         
         
-        assert equal(composed_state,state_ev)
+    assert equal(composed_state,state_ev)
+
 
