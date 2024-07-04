aa0ad7bd27ee0d07df12f35a03517e1bcddb2659
smit <smit.chaudhary@pasqal.com>
2024-07-04 16:54:36 +0200

    Remove redundant test

--- tests/ml_tools/test_qnn.py
+++ tests/ml_tools/test_qnn.py
@@ -347,22 +347,6 @@ def test_variational_transformed_module(
     assert torch.allclose(tm_pred, (output_range[0] * pred) + output_range[1])
 
 
-def test_config_empty_fm() -> None:
-    fm_config = FeatureMapConfig()
-    ansatz_config = AnsatzConfig()
-    observable_config = ObservableConfig(detuning=Z)
-
-    qnn = build_qnn_from_configs(
-        register=2,
-        fm_config=fm_config,
-        ansatz_config=ansatz_config,
-        observable_config=observable_config,
-    )
-
-    assert qnn.in_features == 0
-    assert isinstance(qnn, QNN)
-
-
 @pytest.mark.parametrize("diff_mode", [DiffMode.GPSR, DiffMode.AD])
 @pytest.mark.parametrize("backend", [BackendName.PYQTORCH])
 def test_config_qnn(diff_mode: DiffMode, backend: BackendName) -> None:
