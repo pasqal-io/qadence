5d70e33c22194a3f2316182126ece09a073dbe13
smit <smit.chaudhary@pasqal.com>
2024-08-12 12:51:34 +0200

    Add consistent naming and use str enums for types in constructors utils

--- qadence/constructors/utils.py
+++ qadence/constructors/utils.py
@@ -7,61 +7,59 @@ import sympy
 from qadence.blocks import KronBlock, kron
 from qadence.operations import RY
 from qadence.parameters import FeatureParameter, Parameter
-from qadence.types import PI
+from qadence.types import PI, BasisSet, MultivariateStrategy, ReuploadScaling
 
 
-def generator_prefactor(spectrum: str, qubit_index: int) -> float | int:
+def generator_prefactor(reupload_scaling: ReuploadScaling, qubit_index: int) -> float | int:
     """Converts a spectrum string, e.g. tower or exponential.
 
     The result is the correct generator prefactor.
     """
-    spectrum = spectrum.lower()
     conversion_dict: dict[str, float | int] = {
-        "simple": 1,
-        "tower": qubit_index + 1,
-        "exponential": 2 * PI / (2 ** (qubit_index + 1)),
+        ReuploadScaling.CONSTANT: 1,
+        ReuploadScaling.TOWER: qubit_index + 1,
+        ReuploadScaling.EXP: 2 * PI / (2 ** (qubit_index + 1)),
     }
-    return conversion_dict[spectrum]
+    return conversion_dict[reupload_scaling]
 
 
-def basis_func(basis: str, x: Parameter) -> Parameter | sympy.Expr:
-    basis = basis.lower()
+def basis_func(basis: BasisSet, x: Parameter) -> Parameter | sympy.Expr:
     conversion_dict: dict[str, Parameter | sympy.Expr] = {
-        "fourier": x,
-        "chebyshev": 2 * sympy.acos(x),
+        BasisSet.FOURIER: x,
+        BasisSet.CHEBYSHEV: 2 * sympy.acos(x),
     }
     return conversion_dict[basis]
 
 
 def build_idx_fms(
-    basis: str,
+    basis: BasisSet,
     fm_pauli: Type[RY],
-    fm_strategy: str,
+    multivariate_strategy: MultivariateStrategy,
     n_features: int,
     n_qubits: int,
-    spectrum: str,
+    reupload_scaling: ReuploadScaling,
 ) -> list[KronBlock]:
     """Builds the index feature maps based on the given parameters.
 
     Args:
-        basis (str): Type of basis chosen for the feature map.
+        basis (BasisSet): Type of basis chosen for the feature map.
         fm_pauli (PrimitiveBlock type): The chosen Pauli rotation type.
-        fm_strategy (str): The feature map strategy to be used. Possible values are
-            'parallel' or 'serial'.
+        multivariate_strategy (MultivariateStrategy): The strtegy used for encoding
+            the multivariate feature map.
         n_features (int): The number of features.
         n_qubits (int): The number of qubits.
-        spectrum (str): The chosen spectrum.
+        reupload_scaling (ReuploadScaling): The chosen scaling for the reupload.
 
     Returns:
         List[KronBlock]: The list of index feature maps.
     """
     idx_fms = []
     for i in range(n_features):
-        target_qubits = get_fm_qubits(fm_strategy, i, n_qubits, n_features)
+        target_qubits = get_fm_qubits(multivariate_strategy, i, n_qubits, n_features)
         param = FeatureParameter(f"x{i}")
         block = kron(
             *[
-                fm_pauli(qubit, generator_prefactor(spectrum, j) * basis_func(basis, param))
+                fm_pauli(qubit, generator_prefactor(reupload_scaling, j) * basis_func(basis, param))
                 for j, qubit in enumerate(target_qubits)
             ]
         )
@@ -70,7 +68,9 @@ def build_idx_fms(
     return idx_fms
 
 
-def get_fm_qubits(fm_strategy: str, i: int, n_qubits: int, n_features: int) -> Iterable:
+def get_fm_qubits(
+    multivariate_strategy: MultivariateStrategy, i: int, n_qubits: int, n_features: int
+) -> Iterable:
     """Returns the list of target qubits for the given feature map strategy and feature index.
 
     Args:
@@ -86,11 +86,11 @@ def get_fm_qubits(fm_strategy: str, i: int, n_qubits: int, n_features: int) -> I
     Raises:
         ValueError: If the feature map strategy is not implemented.
     """
-    if fm_strategy == "parallel":
+    if multivariate_strategy == MultivariateStrategy.PARALLEL:
         n_qubits_per_feature = int(n_qubits / n_features)
         target_qubits = range(i * n_qubits_per_feature, (i + 1) * n_qubits_per_feature)
-    elif fm_strategy == "serial":
+    elif multivariate_strategy == MultivariateStrategy.SERIES:
         target_qubits = range(0, n_qubits)
     else:
-        raise ValueError(f"Feature map strategy {fm_strategy} not implemented.")
+        raise ValueError(f"Multivariate strategy {multivariate_strategy} not implemented.")
     return target_qubits
