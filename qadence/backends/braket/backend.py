a959171617387076b69eb620a994f3e024e7de67
João P. Moutinho <joao.moutinho@pasqal.com>
2024-08-27 15:41:06 +0200

    [Refact] Some qadence noise / pulser backend changes (#547)

--- qadence/backends/braket/backend.py
+++ qadence/backends/braket/backend.py
@@ -131,16 +131,6 @@ class Backend(BackendInterface):
             states = invert_endianness(states)
         return states
 
-    def run_dm(
-        self,
-        circuit: ConvertedCircuit,
-        noise: Noise,
-        param_values: dict[str, Tensor] = {},
-        state: Tensor | None = None,
-        endianness: Endianness = Endianness.BIG,
-    ) -> Tensor:
-        raise NotImplementedError
-
     def sample(
         self,
         circuit: ConvertedCircuit,
