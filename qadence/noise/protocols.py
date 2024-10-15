3e5c2f4dd3033982c27db4a0d6e2cdcd020c5c23
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-10-15 16:12:59 +0200

    reputting old noise interface

--- qadence/noise/protocols.py
+++ qadence/noise/protocols.py
@@ -14,6 +14,41 @@ Postprocessing_PROTOCOL_TO_MODULE = {
     "readout": "qadence.noise.readout",
 }
 
+# Temporary solution
+DigitalNoise = pyq.NoiseProtocol
+
+
+@dataclass
+class Noise:
+    DEPHASING = "dephasing"
+    DEPOLARIZING = "depolarizing"
+    READOUT = "readout"
+
+    def __init__(self, protocol: str, options: dict = dict()) -> None:
+        self.protocol: str = protocol
+        self.options: dict = options
+
+    def get_noise_fn(self) -> Callable:
+        try:
+            module = importlib.import_module(Postprocessing_PROTOCOL_TO_MODULE[self.protocol])
+        except KeyError:
+            ImportError(f"The module corresponding to the protocol {self.protocol} is not found.")
+        fn = getattr(module, "add_noise")
+        return cast(Callable, fn)
+
+    def _to_dict(self) -> dict:
+        return {"protocol": self.protocol, "options": self.options}
+
+    @classmethod
+    def _from_dict(cls, d: dict) -> Noise | None:
+        if d:
+            return cls(d["protocol"], **d["options"])
+        return None
+
+    @classmethod
+    def list(cls) -> list:
+        return list(filter(lambda el: not el.startswith("__"), dir(cls)))
+
 
 @dataclass
 class NoiseProtocol:
