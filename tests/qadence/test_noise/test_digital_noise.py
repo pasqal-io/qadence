edbcbbbb03ede0425d4c28f2b7cfce4740296150
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-10-22 11:05:46 +0200

    fix lint

--- tests/qadence/test_noise/test_digital_noise.py
+++ tests/qadence/test_noise/test_digital_noise.py
@@ -1,44 +1,44 @@
 from __future__ import annotations
 
-import pytest
-
-import strategies as st  # type: ignore
-from hypothesis import given, settings
-import torch
-from qadence import set_noise, QuantumCircuit, DigitalNoiseType, DigitalNoise
 import random
 
+import pytest
+import strategies as st  # type: ignore
+from hypothesis import given, settings
+
+from qadence import DigitalNoise, DigitalNoiseType, QuantumCircuit, set_noise
+
 list_noises = [DigitalNoiseType(noise.value) for noise in DigitalNoiseType]
+
 
 @pytest.mark.parametrize("protocol", list_noises)
 @given(st.digital_circuits())
 @settings(deadline=None)
 def test_set_noise(protocol: str, circuit: QuantumCircuit) -> None:
-
-    for block in circuit.block.blocks:
+    all_blocks = circuit.block.blocks if hasattr("blocks", circuit.block) else [circuit.block]  # type: ignore
+    for block in all_blocks:
         assert block.noise is None
-    noise = DigitalNoise(protocol, error_probability = 0.2)
+    noise = DigitalNoise(protocol, error_probability=0.2)
     assert noise.len == 1
     set_noise(circuit, noise)
 
-    for block in circuit.block.blocks:
+    for block in all_blocks:
         assert block.noise is not None
+
 
 @pytest.mark.parametrize("protocol", list_noises)
 @given(st.digital_circuits())
 @settings(deadline=None)
 def test_set_noise_restricted(protocol: str, circuit: QuantumCircuit) -> None:
-
-    noise = DigitalNoise(protocol, error_probability = 0.2)
+    noise = DigitalNoise(protocol, error_probability=0.2)
     assert noise.len == 1
-    index_random_block = random.randint(0, len(circuit.block.blocks) - 1)
-    type_target = type(circuit.block.blocks[index_random_block])
-    set_noise(circuit, noise, target_class=type_target)
+    all_blocks = circuit.block.blocks if hasattr("blocks", circuit.block) else [circuit.block]  # type: ignore
+    index_random_block = random.randint(0, len(all_blocks) - 1)
+    type_target = type(all_blocks[index_random_block])
+    set_noise(circuit, noise, target_class=type_target)  # type: ignore[arg-type]
 
-    for block in circuit.block.blocks:
+    for block in all_blocks:
         if isinstance(block, type_target):
             assert block.noise is not None
         else:
-            if block.noise is not None:
-                print("la merde", block, block.noise, type_target)
             assert block.noise is None
