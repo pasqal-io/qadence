580dfec2b1965f4d29ade953604b56fbeb598dbc
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-10-29 17:46:49 +0100

    add test equality digital noise bitflip

--- tests/qadence/test_noise/test_digital_noise.py
+++ tests/qadence/test_noise/test_digital_noise.py
@@ -104,7 +104,18 @@ def test_append(noise_config: NoiseProtocol | list[NoiseProtocol]) -> None:
     options = {"error_probability": 0.1}
     noise = NoiseHandler(NoiseProtocol.DIGITAL.BITFLIP, options)
 
-    len_noise2 = len(noise_config) if isinstance(noise_config, list) else 1
+    len_noise_config = len(noise_config) if isinstance(noise_config, list) else 1
     noise.append(NoiseHandler(noise_config, options))
 
-    assert len(noise.protocols) == (len_noise2 + 1)
+    assert len(noise.protocols) == (len_noise_config + 1)
+
+
+def test_equality() -> None:
+    options = {"error_probability": 0.1}
+    noise = NoiseHandler(NoiseProtocol.DIGITAL.BITFLIP, options)
+    noise.append(NoiseHandler(NoiseProtocol.DIGITAL.BITFLIP, options))
+
+    noise2 = NoiseHandler(NoiseProtocol.DIGITAL.BITFLIP, options)
+    noise2.bitflip(options)
+
+    assert noise == noise2
