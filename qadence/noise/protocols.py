524a64d137767ab877f57ca2a463acc0271187f2
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-10-30 10:15:26 +0100

    fix docstr too

--- qadence/noise/protocols.py
+++ qadence/noise/protocols.py
@@ -19,7 +19,7 @@ class NoiseHandler:
     exclusively with `NoiseProtocol.DIGITAL` sources.
 
     Args:
-        protocols: The protocol(s) applied. To be defined from `NoiseProtocol`.
+        protocol: The protocol(s) applied. To be defined from `NoiseProtocol`.
         options: A list of options defining the protocol.
             For `NoiseProtocol.ANALOG`, options should contain a field `noise_probs`.
             For `NoiseProtocol.DIGITAL`, options should contain a field `error_probability`.
