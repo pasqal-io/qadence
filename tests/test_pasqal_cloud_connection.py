02c4629671d86f000d72c291e4d1ed720c77cd87
Pim Venderbosch <pim.venderbosch@pasqal.com>
2024-10-30 14:47:06 +0100

    Add tests for get_status function

--- tests/test_pasqal_cloud_connection.py
+++ tests/test_pasqal_cloud_connection.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Callable
+from unittest.mock import Mock
 
 import pytest
 
@@ -11,6 +12,7 @@ from qadence.pasqal_cloud_connection import (
     WorkloadSpec,
     WorkloadStoppedError,
     check_status,
+    get_result,
     upload_workload,
 )
 
@@ -28,9 +30,18 @@ def test_upload_workload(mocker: Any, BasicQuantumModel: QuantumModel) -> None:
     assert result == expected_workload_id
 
 
-def test_check_status_done(mocker: Any) -> None:
-    mock_workload_result = mocker.Mock()
-    mock_workload_result.status = "DONE"
+@pytest.fixture
+def mock_status(mocker: Any) -> Callable:
+    def generate_status(status: str) -> Mock:
+        workload_result: Mock = mocker.Mock()
+        workload_result.status = status
+        return workload_result
+
+    return generate_status
+
+
+def test_check_status_done(mocker: Any, mock_status: Callable) -> None:
+    mock_workload_result = mock_status("DONE")
     mock_connection = mocker.Mock()
     mock_connection.get_workload.return_value = mock_workload_result
     result = check_status(mock_connection, "my-workload")
@@ -49,10 +60,40 @@ def test_check_status_done(mocker: Any) -> None:
         ("weird-status", ValueError),
     ],
 )
-def test_check_status(mocker: Any, status: str, expected_error: Exception) -> None:
-    mock_workload_result = mocker.Mock()
-    mock_workload_result.status = status
+def test_check_status_with_error(
+    mocker: Any, status: str, expected_error: Exception, mock_status: Callable
+) -> None:
+    mock_workload_result = mock_status(status)
     mock_connection = mocker.Mock()
     mock_connection.get_workload.return_value = mock_workload_result
     with pytest.raises(expected_error):
         check_status(mock_connection, "my-workload")
+
+
+def test_get_results(mocker: Any, mock_status: Callable) -> None:
+    mocker.patch("qadence.pasqal_cloud_connection.time.sleep", return_value=None)
+    mock_connection = mocker.Mock()
+    result_done = mock_status("DONE")
+    mock_connection.get_workload.side_effect = [
+        mock_status("PENDING"),
+        mock_status("PENDING"),
+        mock_status("RUNNING"),
+        result_done,
+    ]
+    result = get_result(mock_connection, "my-workload")
+    assert result is result_done
+
+
+def test_get_results_timeout(mocker: Any, mock_status: Callable) -> None:
+    mocker.patch("qadence.pasqal_cloud_connection.time.sleep", return_value=None)
+    connection = mocker.Mock()
+    result_pending = mock_status("PENDING")
+    connection.get_workload.side_effect = [
+        result_pending,
+        result_pending,
+        result_pending,
+        result_pending,
+        result_pending,
+    ]
+    with pytest.raises(TimeoutError):
+        get_result(connection, "my-workload", timeout=4)
