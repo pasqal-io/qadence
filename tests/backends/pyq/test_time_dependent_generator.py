8e2495327fcd28349bb7d77d336883cde0bb7cc9
João P. Moutinho <joao.moutinho@pasqal.com>
2024-11-11 15:13:40 +0100

    [Feature] Allow time-dependent duration to be passed at runtime (#605)

--- tests/backends/pyq/test_time_dependent_generator.py
+++ tests/backends/pyq/test_time_dependent_generator.py
@@ -12,7 +12,7 @@ from pyqtorch.utils import SolverType
 from qadence import AbstractBlock, HamEvo, QuantumCircuit, QuantumModel, Register, run
 
 
-@pytest.mark.parametrize("duration", [0.5, 1.0, 2.0, 5.0])
+@pytest.mark.parametrize("duration", [0.5, 1.0, 2.0])
 @pytest.mark.parametrize("ode_solver", [SolverType.DP5_SE, SolverType.KRYLOV_SE])
 def test_time_dependent_generator(
     qadence_generator: AbstractBlock,
@@ -26,17 +26,17 @@ def test_time_dependent_generator(
     n_steps = 500
 
     # simulate with qadence HamEvo using QuantumModel
-    hamevo = HamEvo(qadence_generator, time_param, duration=duration)
+    hamevo = HamEvo(qadence_generator, time_param)
     reg = Register(2)
     circ = QuantumCircuit(reg, hamevo)
-    model = QuantumModel(circ, configuration={"ode_solver": ode_solver, "n_steps_hevo": n_steps})
-    state_qadence0 = model.run(values={"x": torch.tensor(feature_param_x)})
+    config = {"ode_solver": ode_solver, "n_steps_hevo": n_steps}
+    values = values = {"x": torch.tensor(feature_param_x), "duration": torch.tensor(duration)}
 
-    state_qadence1 = run(
-        hamevo,
-        values={"x": torch.tensor(feature_param_x)},
-        configuration={"ode_solver": ode_solver, "n_steps_hevo": n_steps},
-    )
+    model = QuantumModel(circ, configuration=config)
+    state_qadence0 = model.run(values=values)
+
+    # simulate with qadence.execution
+    state_qadence1 = run(hamevo, values=values, configuration=config)
 
     # simulate with qutip
     t_points = np.linspace(0, duration, n_steps)
