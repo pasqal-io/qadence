7749b4f50b1ddccda3f44d3877d627eca2ae34bc
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2024-11-25 17:05:18 +0100

    removing torch init state

--- tests/backends/pyq/test_time_dependent_generator.py
+++ tests/backends/pyq/test_time_dependent_generator.py
@@ -26,7 +26,6 @@ def test_time_dependent_generator(
     n_steps = 500
 
     psi_0 = qutip.basis(4, 0)
-    psi0_torch = (torch.tensor(psi_0.full()).to(torch.complex128)).reshape(1, -1)
 
     # simulate with qadence HamEvo using QuantumModel
     hamevo = HamEvo(qadence_generator, time_param)
@@ -40,10 +39,10 @@ def test_time_dependent_generator(
     }
 
     model = QuantumModel(circ, configuration=config)
-    state_qadence0 = model.run(values=values, state=psi0_torch)
+    state_qadence0 = model.run(values=values)
 
     # simulate with qadence.execution
-    state_qadence1 = run(hamevo, values=values, configuration=config, state=psi0_torch)
+    state_qadence1 = run(hamevo, values=values, configuration=config)
 
     # simulate with qutip
     t_points = np.linspace(0, duration, n_steps)
@@ -67,19 +66,19 @@ def test_noisy_time_dependent_generator(
 ) -> None:
     n_steps = 500
     ode_solver = SolverType.DP5_ME
-
-    # initial state
-    psi_0 = qutip.basis(4, 0)
-    psi0_torch = (torch.tensor(psi_0.full()).to(torch.complex128)).reshape(1, -1)
+    n_qubits = 2
+    n_qubits_pow2 = 2**n_qubits
 
     # Define jump operators
     # Note that we squeeze to remove the batch dimension
-    list_ops = [torch.eye(4, dtype=torch.complex128)]
+    list_ops = [torch.eye(n_qubits_pow2, dtype=torch.complex128)]
 
     # simulate with qadence HamEvo using QuantumModel
     hamevo = HamEvo(qadence_generator, time_param, noise_operators=list_ops)
-    reg = Register(2)
+    reg = Register(n_qubits)
     circ = QuantumCircuit(reg, hamevo)
+    n_qubits = circ.n_qubits
+
     config = {"ode_solver": ode_solver, "n_steps_hevo": n_steps}
     values = {
         "x": torch.tensor(feature_param_x),
@@ -88,15 +87,15 @@ def test_noisy_time_dependent_generator(
     }
 
     model = QuantumModel(circ, configuration=config)
-    state_qadence0 = model.run(values=values, state=psi0_torch)
+    state_qadence0 = model.run(values=values)
 
     # simulate with qadence.execution
-    state_qadence1 = run(hamevo, values=values, configuration=config, state=psi0_torch)
+    state_qadence1 = run(hamevo, values=values, configuration=config)
 
     # simulate with qutip
     t_points = np.linspace(0, duration, n_steps)
-    list_ops_qutip = [qutip.qeye(4)]
-    result = qutip.mesolve(qutip_generator, psi_0, t_points, list_ops_qutip)
+    list_ops_qutip = [qutip.qeye(n_qubits_pow2)]
+    result = qutip.mesolve(qutip_generator, qutip.basis(n_qubits_pow2, 0), t_points, list_ops_qutip)
 
     state_qutip = torch.tensor(result.states[-1].full()).unsqueeze(0)
     assert torch.allclose(state_qadence0, state_qutip, atol=MIDDLE_ACCEPTANCE)
