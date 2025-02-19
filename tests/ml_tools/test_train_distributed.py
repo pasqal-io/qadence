7895ed2441f7df904dbcd0dc96f168f3c6294e10
mlahariya <40852060+mlahariya@users.noreply.github.com>
2025-02-19 14:20:37 +0100

    Update distributed test

--- tests/ml_tools/test_train_distributed.py
+++ tests/ml_tools/test_train_distributed.py
@@ -79,6 +79,7 @@ def test_train_spawn(Basic: nn.Module, backend: str, device: str) -> None:
 
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = str(random.randint(10000, 60000))
+    os.environ["MASTER_ADDR"] = str(nprocs)
 
     config: TrainConfig = TrainConfig(
         compute_setup=device,
@@ -125,6 +126,7 @@ def test_train_spawn_dictdataloader(Basic: nn.Module, backend: str, device: str)
 
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = str(random.randint(10000, 60000))
+    os.environ["MASTER_ADDR"] = str(nprocs)
 
     config: TrainConfig = TrainConfig(
         compute_setup=device,
