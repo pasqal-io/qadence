31c66faa812b632bb0fd5b90f2f7af7e083add17
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-02-25 10:20:20 +0100

    fix lint

--- tests/test_str_utils.py
+++ tests/test_str_utils.py
@@ -14,7 +14,10 @@ import torch
         (Z(0), "Z(0)"),
         (MatrixBlock(torch.tensor([[0, 1], [1, 0]], dtype=torch.cdouble), (0,)), "MatrixBlock(0)"),
         (hamiltonian_factory(2, detuning=Z), "(Z(0) + Z(1))"),
-        (2.0 * hamiltonian_factory(2, detuning=Z) + kron(Z(0), Z(1)), "(2.000 * (Z(0) + Z(1)) + (Z(0) ⊗ Z(1)))"),
+        (
+            2.0 * hamiltonian_factory(2, detuning=Z) + kron(Z(0), Z(1)),
+            "(2.000 * (Z(0) + Z(1)) + (Z(0) ⊗ Z(1)))",
+        ),
     ],
 )
 def test_blocktree_to_mathematical_expression(block: AbstractBlock, expected_str: str) -> None:
