78085eb9948e1eb2766f62f32d4c66192ef798d1
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-03-20 09:30:55 +0100

    fix classification tutorial with train test split

--- examples/models/qnn_classification.py
+++ examples/models/qnn_classification.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 import random
 
-import mlflow
 import torch
 import torch.nn as nn
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from torch import Tensor
 from torch.utils.data import DataLoader, Dataset
 
@@ -15,12 +15,23 @@ from qadence.ml_tools import TrainConfig, Trainer
 
 
 class IrisDataset(Dataset):
+    """The Iris dataset split into a training set and a test set.
+
+    A StandardScaler is applied prior to applying models.
+    """
+
     def __init__(self):
         X, y = load_iris(return_X_y=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
         scaler = StandardScaler()
-        self.X = torch.tensor(scaler.fit_transform(X), requires_grad=False)
-        self.y = torch.tensor(y, requires_grad=False)
+        scaler.fit(X_train)
+        self.scaler = scaler
+        self.X = torch.tensor(scaler.transform(X_train), requires_grad=False)
+        self.y = torch.tensor(y_train, requires_grad=False)
+
+        self.X_test = torch.tensor(scaler.transform(X_test), requires_grad=False)
+        self.y_test = torch.tensor(y_test, requires_grad=False)
 
     def __getitem__(self, index) -> tuple[Tensor, Tensor]:
         return self.X[index], self.y[index]
@@ -29,25 +40,21 @@ class IrisDataset(Dataset):
         return len(self.y)
 
 
-def main():
+if __name__ == "__main__":
     n_features = 4  # sepal length, sepal width, petal length, petal width
     n_layers = 3
     n_neurons_final_linear_layer = 3
     n_epochs = 1000
     lr = 1e-1
-
-    mlflow.log_params(
-        {
-            "n_layers_reuploading": n_layers,
-            "n_qubits": n_features,
-            "n_neurons_final_linear_layer": n_neurons_final_linear_layer,
-            "n_epochs": n_epochs,
-            "lr": lr,
-        }
-    )
     dataset = IrisDataset()
 
     dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
+
+    # 1. Set up hybrid QNN model
+
+    ## 1.1 Set up the QNN part composed of multiple feature map layers
+    ## each followed by a variational layer
+    ## The output will be the expectation value wrt a Z observable on qubit 0
 
     feature_parameters = [FeatureParameter(f"x_{i}") for i in range(n_features)]
     fm_layer = RX(0, feature_parameters[0])
@@ -63,12 +70,15 @@ def main():
         blocks = chain(blocks, fm_layer, ansatz_layers[layer])
 
     qc = QuantumCircuit(n_features, blocks)
-    # savefig(qc, "qc.png")
     qnn = QNN(circuit=qc, observable=Z(0), inputs=[f"x_{i}" for i in range(n_features)])
 
-    model = nn.Sequential(qnn, nn.Linear(1, n_neurons_final_linear_layer), nn.Softmax(dim=1))
-    opt = torch.optim.Adam(model.parameters(), lr=lr)
+    ## 1.2 Augment the QNN with a simple linear layer as classification head
+    ## Note softmax is not applied with the cross-entropy loss
+    model = nn.Sequential(qnn, nn.Linear(1, n_neurons_final_linear_layer))
 
+    # 2. Set up loss and training
+
+    opt = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
 
     def cross_entropy(model: nn.Module, data: Tensor) -> tuple[Tensor, dict]:
@@ -82,19 +92,10 @@ def main():
 
     trainer.fit(dataloader)
 
-    # Final accuracy on a random set of points
-    idx_test = random.sample(range(len(dataset)), 50)
-    X_test, y_test = dataset[idx_test]
+    # 3. Apply model and test set
 
-    print((torch.argmax(model(X_test), dim=1) == y_test).type(torch.float32).mean().item())
-    accuracy_test = (torch.argmax(model(X_test)) == y_test).type(torch.float32).mean()
-
-    mlflow.log_metric("Test Accuracy", accuracy_test.item())
-
-    mlflow.pytorch.log_model(
-        model, artifact_path="final_model", input_example=X_test.detach().numpy()
-    )
-
-
-if __name__ == "__main__":
-    main()
+    X_test, y_test = dataset.X_test, dataset.y_test
+    preds_test = torch.argmax(torch.softmax(model(X_test), dim=1), dim=1)
+    accuracy_test = (preds_test == y_test).type(torch.float32).mean()
+    ## Should reach higher than 0.9
+    print("Test Accuracy", accuracy_test.item())
