d2fac5f3412f7e04587bed2419002ed5d7f7dbca
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-06-18 14:02:38 +0200

    linting

--- tests/qadence/test_noise/test_lshift.py
+++ tests/qadence/test_noise/test_lshift.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from qadence import RX, X, Y
 from qadence.noise import available_protocols
 
+
 def test_equality() -> None:
     noise = available_protocols.Bitflip(error_definition=0.1)
-    rx = RX(0, 'theta')
-    rx_noise = RX(0, 'theta', noise=noise)
+    rx = RX(0, "theta")
+    rx_noise = RX(0, "theta", noise=noise)
     assert rx != rx_noise
 
     rx = rx << noise
